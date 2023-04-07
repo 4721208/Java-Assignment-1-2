382d805490216e0246279b5e33e439b6e302aeb4
4721208 <130113245+4721208@users.noreply.github.com>
2023-04-07 11:01:09 +0900

    Update Prog12.java

--- src/main/java/Prog12.java
+++ src/main/java/Prog12.java
@@ -1,3 +1,9 @@
+/**
+ * クラス Prog12
+ * 
+ * @author (4721208　佐藤友麻)
+ * @version (2022/04/07)
+ */
 public class Prog12 {
 
 	public static void main(String[] args) {
@@ -7,10 +13,9 @@ public class Prog12 {
 		System.out.println(pie * pie * PI);
 
 		System.out.println("パイの半径を倍にします");
-		PI = 10;
+		pie = 10;
 		System.out.println("半径" + pie + "cmのパイの面積は、");
 		System.out.println(pie * pie * PI);
 	}
 
 }
-
